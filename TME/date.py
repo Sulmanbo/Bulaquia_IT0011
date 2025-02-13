47cec4856609096afc02c049ae8f4b5aee0bcf51
Sulmanbo <161391449+Sulmanbo@users.noreply.github.com>
2025-02-13 22:28:19 +0800

    Delete TME/date.py

--- TME/date.py
+++
@@ -1,9 +0,0 @@
-#Create a program that will translate a given date format in mm/dd/yyyy to more human readable
-
-Months_list = ["January", "February", "March", "April", "May", "June", 
-               "July", "August", "September", "October", "November", "December"]
-
-month, day, year = input("Enter the date (MM/DD/YYYY): ").split("/")
-month_name = Months_list[int(month) - 1]
-
-print(f"Date Output: {month_name} {int(day)}, {year}")
